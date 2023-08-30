2f3e8646be2b86ed26b90e9f6319f8bc2d669d3f
garnold02 <gulyas.arnold02@protonmail.com>
2023-08-30 21:30:08 +0200

    Update GitHub link

--- src/components/GitHub.test.tsx
+++ src/components/GitHub.test.tsx
@@ -8,6 +8,8 @@ afterEach(cleanup);
 describe("GitHub", () => {
     it("should display the GitHub link", async () => {
         render(<GitHub />);
-        expect(screen.getByRole("link").getAttribute("href")).toBe("https://github.com/garnold02");
+        expect(screen.getByRole("link").getAttribute("href")).toBe(
+            "https://github.com/garnold02/moviesearch"
+        );
     });
 });
